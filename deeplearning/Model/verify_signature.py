83498166e6802a2015f8a52a8e09c29ce0b339c1
satvik2106 <satvik.vattipalli1311@gmail.com>
2024-12-27 19:11:35 +0530

    Update verify_signature.py

--- deeplearning/Model/verify_signature.py
+++ deeplearning/Model/verify_signature.py
@@ -125,5 +125,5 @@ def verify_signature():
 
 if __name__ == "__main__":
     # Get the port from the environment variable or default to 5000
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT",     10000))
     app.run(host="0.0.0.0", port=port, debug=True)  # Set debug=True only for development
